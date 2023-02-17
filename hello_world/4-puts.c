bc66b69f715154abf4b7f40c0c781ec5f1e5c253
madja0125 <dejesusmiguel788@gmail.com>
2023-02-17 18:18:06 +0000

    clow level

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("Programming is like building a multilinngual puzzle\n");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
