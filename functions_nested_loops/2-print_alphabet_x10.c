e62042ac3f79951cddd45405404c62d9136ae791
madja0125 <dejesusmiguel788@gmail.com>
2023-02-22 21:55:12 +0000

    putchar

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print alphabet x10
- * 3
+ * print alphabet x10 -  a function that prints 10 times the alphabet
+ * 
  * Return: x10 a-z
  */
 void print_alphabet_x10(void)
