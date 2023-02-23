2e3206a2a35bc17696487ee9fbb41bf4a22e58bd
madja0125 <dejesusmiguel788@gmail.com>
2023-02-23 15:19:10 +0000

    loops

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,16 +8,15 @@ int print_last_digit(int r)
 {
 	int a;
 
-	a = r % 10;
-
-	if (a < 0)
-	{
-		_putchar (-1 + 48);
-	return:-1
-	}
-	else 
-	{
-		_putchar(1 + 48);
-	return:1
-	}
+	a = r % 10
+		if (a < 0)
+		{
+			_putchar(-1 + 48);
+			return: -1
+		}
+		else
+		{
+			_putchar(1 + 48);
+			return: 1
+		}
 }
