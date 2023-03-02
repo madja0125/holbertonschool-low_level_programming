751ce05047ffe41bd39ed1473f07548dcd78db21
madja0125 <dejesusmiguel788@gmail.com>
2023-03-02 21:55:31 +0000

    array

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -5,15 +5,18 @@
  * @s2: input two
  * Return: 0  Always
  */
+
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i, op = 0;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	while (op == 0)
 	{
-		if (s1[i] != s2[i]
-				return (s1[i] - s2[1]);
-				}
+		if ((*(s1 + i) == '\0') && (*(s2 + i) =='\0'))
+			break;
+		op = *(s1 + i) - *(s2 + i);
+		i++;
+	}
 
-				return (0);
-				}
+	return (op);
+}
