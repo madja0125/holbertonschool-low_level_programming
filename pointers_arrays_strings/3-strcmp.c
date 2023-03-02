e7cfbf5fde21082ab17f2d85decc577d2aff1e97
madja0125 <dejesusmiguel788@gmail.com>
2023-03-02 18:19:25 +0000

    array

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * strncmp - a function that compares two strings
+ * @s1: input one
+ * @s2: input two
+ * Return: 0  Always
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i]
+				return (s1[i] - s2[1]);
+				}
+
+				return (0);
+				}
