7ecb9f642917bf1625e22821a086340f6aa30d5d
madja0125 <dejesusmiguel788@gmail.com>
2023-03-10 02:49:17 +0000

    argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints its name
+ * @argc: number of argument typed
+ * @argv: array pointing to arguments
+ * Return 0
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
+
