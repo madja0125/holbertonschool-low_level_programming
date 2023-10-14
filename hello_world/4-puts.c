a3283f753d2c782c9e9c264d6560e6e03e0bd119
madja0125 <dejesusmiguel788@gmail.com>
2023-10-14 16:30:08 +0000

    fixing number 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+/**
+ * main -entry 
+ * Return Always (0) (Succesfful)
+ */
 
 int main(void)
 {
-	puts("Programmin is like building a multilingual puzzle\n");
+	puts("Programmin is like building a multilingual puzzle");
 
-	return 0;
+	return (0);
 }
