99083b5f854fdb706293984da237ac9f65692816
madja0125 <dejesusmiguel788@gmail.com>
2023-10-14 21:58:23 +0000

    adding number 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * Main - is the entry point of the program
+ * Return: (0) (Always Suscesful)
+ */
+
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
