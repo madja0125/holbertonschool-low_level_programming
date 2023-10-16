cade0bd14911a5d509d045b67545f8ec36d779f0
madja0125 <dejesusmiguel788@gmail.com>
2023-10-16 21:58:40 +0000

    adding number 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *
+ * main - entry point of the program
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 97; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	
+	return (0);
+}
