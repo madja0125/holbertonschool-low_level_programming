e9ba64843044b56a80a91d5f1a6ad9e0ec33e31f
madja0125 <dejesusmiguel788@gmail.com>
2023-10-16 18:25:50 +0000

    adding number 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point of the program
+ * Return: always (0)
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 97; i < 122; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+		
+	}
+	putchar('\n');
+	return (0);
+}
