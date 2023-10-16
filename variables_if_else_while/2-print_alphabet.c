ab542265179007a74d1d9cc7ddb3c743621b8416
madja0125 <dejesusmiguel788@gmail.com>
2023-10-16 14:39:44 +0000

    adding number 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: (0)
+ */
+
+int main(void)
+{
+	char alp[26]="abcdefghijklmnopqstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+
+	return (0);
+}
