c6cbb427857d95ae8ec93f69118c82928b27d6c1
madja0125 <dejesusmiguel788@gmail.com>
2023-10-17 17:54:36 +0000

    adding 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - entry point calculates the absolute integer
+ * @m: the variable
+ * Return: Always (0)
+ */
+
+int _abs(int m)
+{
+	if (m > 0)
+		return (m);
+	else
+		return (m * -1);
+}
+
