dc7a31a7a874cd2fe874aa5626b4d4e9c166ee0f
madja0125 <dejesusmiguel788@gmail.com>
2023-10-22 15:49:45 +0000

    adding 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - starting point
+ * Return: 0 always
+ * @n: variable
+ */
+
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < a; b++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
