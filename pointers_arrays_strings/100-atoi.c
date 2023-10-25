56c530748d281b13296bd74e9aaccb0ad1576a86
madja0125 <dejesusmiguel788@gmail.com>
2023-10-25 17:30:03 +0000

    adding 10

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - starting point
+ * @s: variable
+ * Return: 0 always
+ */
+
+int _atoi(char *s)
+{
+	int a = 1;
+	int b = 0;
+
+	while (*s != '\0')
+	{
+		if (*s == '-')
+		{
+			a = -a;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			b = b * 10 + (*s - '0');
+		}
+		else if (b > 0)
+		{
+			break;
+		}
+		s++;
+	}
+	return (a * b);
+}
