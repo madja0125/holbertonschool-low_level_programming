8685c64290aed216fb46aaf148a2112e0c2bb9cd
madja0125 <dejesusmiguel788@gmail.com>
2023-10-31 20:46:58 +0000

    adding 3 fact

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,19 +1,23 @@
 #include "main.h"
 
 /**
- * factorial - returns the factorial of a given number.
- * @n: input number.
- * Return: factorial of the number.
+ * factorial - function that return the factorial
+ * @n: variable with num
+ * Return: 1 is above 0 and return -1 if lower than 0
  */
 
 int factorial(int n)
 {
-	if (n <= 0)
-		return (-1);
-
-	else if (n == 0)
+	if (n == 0)
+	{
 		return (1);
-
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
 	else
-		return (n * factorial(n -1));
+	{
+		return (n * factorial(n - 1));
+	}
 }
