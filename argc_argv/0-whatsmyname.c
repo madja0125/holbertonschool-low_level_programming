fefe4c9984e464c9a35b6f42b684ec4536b201a1
madja0125 <dejesusmiguel788@gmail.com>
2023-11-06 02:13:23 +0000

    adding 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - starting point
+ * @argv: argument vector
+ * @argc: argument counter
+ * Return: Integer
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
