12d691c361d60d3a4aa04e8e5bd6b145656ba77a
madja0125 <dejesusmiguel788@gmail.com>
2023-11-13 03:20:39 +0000

    fixing

--- preprocessor/4-sum.h
+++ preprocessor/4-sum.h
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
-#ifdef SUM_H
-=======
-#ifndef SUM_H
->>>>>>> 37d8a1612eb3c4f7971ffa22a143e642d689ac2d
-#define SUM_H
+#ifndef _SUM_LIKE_MACRO_
+#define _SUM_LIKE_MACRO_
+
 #define SUM(x, y) ((x) + (y))
+
 #endif
